4ecc3306e12c94411355587062242412e0c59f49
jason <jason@MSI>
2023-01-28 19:38:40 -0600

    Add project files.

---
+++ LAIRS/LAIRS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAIRS
+{
+    internal class LAIRS
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
